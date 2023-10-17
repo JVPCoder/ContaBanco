a593aa124c2d766f800e22b02e9d8a6138169acb
JVPCoder <doublecanal@gmail.com>
2023-10-17 11:48:18 -0300

    Retirada a classe Teste para melhor leitura
    Retirei a classe teste, tendo em vista que é uma implementação simples

--- src/banco/ContaTerminal.java
+++ src/banco/ContaTerminal.java
@@ -20,10 +20,13 @@ public class ContaTerminal {
 		Scanner entrada = new Scanner(System.in);
 		System.out.println("Por favor digite o numero da conta:");
 		this.numero = entrada.nextInt();
+		
 		System.out.println("Agora digite a Agência:");
 		this.agencia = entrada.next();
+		
 		System.out.println("Digite o nome do cliente:");
 		this.cliente = entrada.next();
+		
 		System.out.println("Por fim, digite o saldo:");
 		this.saldo = entrada.nextDouble();
 		
@@ -31,7 +34,19 @@ public class ContaTerminal {
 	}
 	
 	public void informarConta() {
-		System.out.println("Olá " + this.cliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + this.agencia + ", conta " + this.numero + " e seu saldo de R$" + this.saldo + " já está disponível para saque.");	
+		System.out.println(
+				"Olá " + this.cliente + ","
+				+ " obrigado por criar uma conta em nosso banco, sua agência é " + this.agencia + ","
+				+ " conta " + this.numero + ""
+				+ " e seu saldo de R$" + this.saldo + " já está disponível para saque."
+				);	
+	}
+	
+	public static void main(String[] args) {
+		ContaTerminal conta1 = new ContaTerminal();
+		
+		conta1.criarConta();
+		conta1.informarConta();
 	}
 
 }
