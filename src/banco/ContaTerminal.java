05b0ae0ba8a8a9423cf6aff0e996d19e471d256f
JVPCoder <doublecanal@gmail.com>
2023-10-17 11:51:55 -0300

    Adicionando Getters e Setters

--- src/banco/ContaTerminal.java
+++ src/banco/ContaTerminal.java
@@ -49,4 +49,37 @@ public class ContaTerminal {
 		conta1.informarConta();
 	}
 
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(String agencia) {
+		this.agencia = agencia;
+	}
+
+	public String getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(String cliente) {
+		this.cliente = cliente;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	
 }
